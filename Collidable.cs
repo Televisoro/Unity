d90604c6ec62db7593ecbe3a6b78206ec010a883
Galimberti Tommaso <galimberti.tommaso@osdbsesto>
2022-03-18 16:10:03 +0100

    1:45:40

--- Collidable.cs
+++ Collidable.cs
@@ -9,4 +9,17 @@ public class Collidable : MonoBehaviour
     private BoxCollider2D boxCollider;
     private Collider2D[] hits = new Collider2D[10];
 
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    protected virtual void Update()
+    {
+        //collision work
+        BoxCollider.OverlapCollider(filter,hits);
+        for(int i=0; i<hits.Lenght; i++){
+
+        }
+    }
 }
